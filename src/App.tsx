61c229cd0887b4bb66b2cc0685a3a6992060abdb
Cleison Carlos <cleisonspider@yahoo.com.br>
2025-02-12 15:23:31 -0300

    Telas de Login e de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from 'react-router-dom'
 import Home from './pages/home'
-import Login from './pages/login'
+import {Login} from './pages/login'
 import Register from './pages/register'
 import Dashboard from './pages/dashboard'
 import New from './pages/dashboard/new'
